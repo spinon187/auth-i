c9db1ee6f1e2a1d1e13a24f3a1970eda039670d4
spinon187 <spinon187@gmail.com>
2019-02-25 14:57:55 -0600

    auth-i mvp

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+const Users = require('./data/db.js');
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+  
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+server.post('/api/login', (req, res) => {
+    let {username, password} = req.body;
+  
+    Users.getBy({username})
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({message: `Welcome ${user.username}!`});
+            }
+            else {
+                res.status(401).json({message: 'You shall not pass!'});
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+function restricted(req, res, next) {
+    const { username, password } = req.headers;
+  
+    if (username && password) {
+        Users.getBy({ username })
+            .first()
+            .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(401).json({message: 'You shall not pass!'});
+            }
+            })
+            .catch(error => {
+            res.status(500).json({message: error});
+            });
+    }
+    else {
+        res.status(400).json({message: 'You shall not pass!'});
+    }
+}
+
+server.get('/api/users', restricted, (req, res) => {
+    Users.get()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`\n** Main screen turn on ${port} **\n`));
